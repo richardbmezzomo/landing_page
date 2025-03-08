65669962daa113c7de41ded93158fcbfdc514f2b
richardbmezzomo <richardmezzomo1@gmail.com>
2025-03-08 16:09:38 -0300

    feat: add portfolio, budget and footer

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuLinks = document.querySelectorAll(".menu a");
+
+    menuLinks.forEach(link => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault(); // Evita o comportamento padrão
+
+            const targetId = this.getAttribute("href"); // Pega o ID do destino
+            const targetElement = document.querySelector(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop - 80, // Ajuste para o header fixo
+                    behavior: "smooth" // Ativa a rolagem suave
+                });
+            }
+        });
+    });
+});
